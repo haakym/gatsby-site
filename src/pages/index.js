56a7c8f350fab2df5aa445c3c09e8843974baf1b
Dominic <haakym@gmail.com>
2017-09-13 15:07:09 +0100

    list all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+
+      <section>
+        <h3>Site Pages</h3>
+        <ul>
+          {data.allSitePage.edges.map(({node}) => 
+            <li key={node.path}>
+              <Link to="/{node.path}/">{node.path}</Link>
+            </li>
+          )}
+        </ul>
+      </section>
+      
+    </div>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexPageQuery {
+    allSitePage {
+      edges {
+        node {
+          id
+          path
+        }
+      }
+    }
+  }
+`
